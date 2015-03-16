c5133eeb3af3b7007016711b1dcade066a487eab
Aditya Purwa <adityapurwa@windowslive.com>
2015-03-16 16:27:43 +0700

    Create base formatter and add nuget restore

---
+++ QuranXmlTo/Formatter/IQuranFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuranXmlTo.Formatter
+{
+    /// <summary>
+    /// Interface that defines a quran formatter.
+    /// </summary>
+    public interface IQuranFormatter
+    {
+    }
+}
